66033db0c7c00879f64d772e01869b5686822d7e
Beato Mattia <mattia.beato@cz.lan>
2025-11-04 08:21:47 +0100

    ex done

--- 28_10/001.sql
+++ 28_10/001.sql
@@ -64,6 +64,7 @@ join courses c on c.course_code = f.course_code
 where c.course_name = "SBC"
 order by s.name;
 
+
 /*2. Il nome ed il Dipartimento dei docenti di “Basi di Dati e Sistemi Informativi” o di
 “Informatica Generale”
 */
@@ -71,6 +72,7 @@ select p.name, p.department from professors p
 join courses c on c.professor = p.code
 where c.course_name = "Basi di Dati e Sistemi Informativi" 
 or c.course_name = "Informatica Generale";
+
 
 /*3. Matricola e nome degli studenti di un corso di laurea triennale (tipoLaurea = 'L')
 che seguono un corso di un docente di nome Felice.
@@ -110,16 +112,32 @@ join students s on s.mat_number = f.mat_number
 where p.department like "Informatica"
 having COUNT(s.name) > 5;  
 
+
 /*7. Per ogni studente della Facoltà di Lettere e Filosofia, la matricola ed il numero
 di corsi seguiti
 */
 select s.mat_number, count(c.course_code) from students s
-join 
+join degree_courses dc on s.degree_course = dc.degree_course
+join frequency f on f.mat_number = s.mat_number
+join courses c on c.course_code = f.course_code
+where dc.faculty like "Lettere" or dc.faculty like "Filosofia"
+
+
 
 /*8. Matricola e nome degli studenti che non frequentano nessun corso
 */
+select s.mat_number, s.name from students s 
+join degree_courses dc on s.degree_course = dc.degree_course
+join frequency f on f.mat_number = s.mat_number
+join courses c on c.course_code = f.course_code
+having count(c.course_code) = 0;
 
 
 /*9. Il Codice ed il Nome dei docenti dei Corsi che non sono frequentati da nessuno
 studente
 */
+select p.code, p.name from professors p 
+join courses c on c.professor = p.code
+join frequency f on f.course_code = c.course_code
+join students s on s.mat_number = f.mat_number
+having count(c.course_code) = 0
