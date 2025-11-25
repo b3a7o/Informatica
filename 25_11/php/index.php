1bfc843b15f4a9fb578f47bef0a68c1b512552bc
Beato Mattia <mattia.beato@cz.lan>
2025-11-25 08:44:23 +0100

    php form project started

---
+++ 25_11/php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>LOG IN FORM</h1>
+    <form action="welcome.php" method='POST'>
+        Name: <input type="text", name="name" required><br>
+        Surname: <input type="text" name="surname" required><br>
+        Email: <input type="text", name="email" required><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
