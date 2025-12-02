ea42213ffb9801a15bf3d9cb646196003e5d4d94
Beato Mattia <mattia.beato@cz.lan>
2025-12-02 08:17:41 +0100

    db created - project scheme created

---
+++ 28_11_db_php_1/init_db.sql
@@ -0,0 +1,25 @@
+create DATABASE esPDO
+
+use esPDO
+
+
+create table customers(
+    customer_id int not null AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(127) not null,
+    email VARCHAR(255) not null
+)
+
+create table products(
+    product_id int not null AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) not null,
+    is_available BOOLEAN 
+)
+
+create table orders(
+    order_id int not null AUTO_INCREMENT PRIMARY KEY,
+    product_id int not null,
+    customer_id int not NULL,
+    quantity int not null,
+    Foreign Key (product_id) REFERENCES products(product_id),
+    Foreign Key (customer_id) REFERENCES customers(customer_id)
+)
